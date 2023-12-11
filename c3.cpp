a9d3e796e6d1e3693f5e3c76d04fedffedcee4a6
Value <53051272+Special-JiaHao@users.noreply.github.com>
2023-12-11 11:29:18 +0800

    Create c3.cpp

---
+++ c3.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <mysql/mysql.h>
+#include <cstring>
+#include <cstdio>
+#include <sys/socket.h>
+#include <memory>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+#include "server.h"
+#include "client.h"
+int main()
+{
+    Client client("127.0.1.1", 8852);
+    client.connectSystem("127.0.0.1", 8899);
+    return 0;
+}
+
