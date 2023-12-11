1d5036fde4c898637472a30dad49d1a1dd9400fa
Value <53051272+Special-JiaHao@users.noreply.github.com>
2023-12-11 11:28:59 +0800

    Create c2.cpp

---
+++ c2.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <mysql/mysql.h>
+#include <cstring>
+#include <cstdio>
+#include <sys/socket.h>
+#include <memory>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+#include "server.h"
+#include "client.h"
+int main()
+{
+    Client client("127.0.1.1", 8851);
+    client.connectSystem("127.0.0.1", 8899);
+    return 0;
+}
